b6a0af90070c3316245672f0360ad350c2572e12
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-01-17 08:45:56 -0800

    Will edit first task

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,6 +9,19 @@ import { Component } from '@angular/core';
     <ul>
       <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask()">Edit!</button></li>
     </ul>
+    <hr>
+    <div>
+      <h3>{{selectedTask.description}}</h3>
+      <p>Task Complete? {{selectedTask.done}}</p>
+      <h3>Edit Task</h3>
+      <label>Enter a Task Description</label>
+      <input [(ngModel)]="selectedTask.description">
+      <label>Enter Task Priority (1-3):</label>
+      <br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (Low Priority)
+    </div>  
   </div>
   `
 })
@@ -24,6 +37,8 @@ export class AppComponent {
      new Task('Begin brainstorming possible JavaScript group projects', 2),
      new Task('Add README file to last few Angular repos on GitHub', 1)
   ];
+
+  selectedTask: Task = this.tasks[0];
 
   editTask() {
     alert("You just requested to edit a Task!");
