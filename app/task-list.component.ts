e6da9ba846a42b6a833ddb3412b97a2f5e1704be
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-01-18 20:51:09 -0800

    Edit refactored

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -1,21 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from './task.model';
 
 @Component({
   selector: 'task-list',
   template: `
   <ul>
-    <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
+    <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList">{{currentTask.description}} <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button></li>
   </ul>
   `
 })
 
 export class TaskListComponent {
-  tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course', 3),
-    new Task('Begin brainstorming possible JavaScript group projects', 2),
-    new Task('Add README file to last few Angular repos on GitHub', 2)
-  ];
+  @Input() childTaskList: Task[];
+  @Output() clickSender = new EventEmitter();
+
+  editButtonHasBeenClicked(taskToEdit: Task) {
+    this.clickSender.emit(taskToEdit);
+  }
 
   isDone(clickedTask: Task) {
     if(clickedTask.done === true) {
@@ -25,13 +26,13 @@ export class TaskListComponent {
     }
   }
 
-  priorityColor(currentTask){
-    if (currentTask.priority === 3){
-      return "bg-danger";
-    } else if (currentTask.priority === 2) {
-      return  "bg-warning";
-    } else {
-      return "bg-info";
-    }
-  }
+  // priorityColor(currentTask){
+  //   if (currentTask.priority === 3){
+  //     return "bg-danger";
+  //   } else if (currentTask.priority === 2) {
+  //     return  "bg-warning";
+  //   } else {
+  //     return "bg-info";
+  //   }
+  // }
 }
